ec1fe1a0802c9675c4537d4999d4d657490fbf02
Franchini Mattia <Franchini.19129>
2023-12-05 16:02:36 +0100

    backend\server.js: created an express server

---
+++ backend/server.js
@@ -0,0 +1,23 @@
+const express = require("express");
+const app = express();
+const cors = require("cors");
+
+//configuration file
+require("dotenv").config({ path: "config/config.env" });
+const port = process.env.PORT || 5000;
+
+app.use(cors());
+
+app.use(express.json());
+
+
+// import db function from ./db/conn.js
+//const dbo = require("./db/conn");
+ 
+app.listen(port, () => {
+  /*dbo.connectToServer(function (err) {
+    if (err) console.error(err);
+ 
+  });*/
+  console.log(`Server is running on port: ${port}`);
+});
